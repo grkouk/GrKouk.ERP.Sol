9c26116a2a8e20f745e349c962664605a9a735c7
GrKouk <grkouk@hotmail.com>
2020-05-18 15:51:27 +0300

    Changes to series changed in web api address

--- GrKouk.Web.ERP/Pages/Index.cshtml.cs
+++ GrKouk.Web.ERP/Pages/Index.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
@@ -11,9 +12,10 @@ namespace GrKouk.Web.ERP.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
+        private readonly SignInManager<IdentityUser> _signInManager;
+        public IndexModel(SignInManager<IdentityUser> signInManager,ILogger<IndexModel> logger)
         {
+            _signInManager = signInManager;
             _logger = logger;
         }
 
