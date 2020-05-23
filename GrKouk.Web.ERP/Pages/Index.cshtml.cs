98ad00c81bfc53d6795479ee68c4bfe36eef0839
GrKoukRider <grkouk1966@gmail.com>
2020-05-23 17:00:25 +0300

    Main Info page step 1

--- GrKouk.Web.ERP/Pages/Index.cshtml.cs
+++ GrKouk.Web.ERP/Pages/Index.cshtml.cs
@@ -2,26 +2,40 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GrKouk.Web.ERP.Data;
+using GrKouk.Web.ERP.Helpers;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace GrKouk.Web.ERP.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
-        private readonly SignInManager<IdentityUser> _signInManager;
-        public IndexModel(SignInManager<IdentityUser> signInManager,ILogger<IndexModel> logger)
+        private readonly ApiDbContext _context;
+
+        public IndexModel(ApiDbContext context)
         {
-            _signInManager = signInManager;
-            _logger = logger;
+            _context = context;
         }
 
         public void OnGet()
         {
+            LoadFilters();
+        }
 
+        private void LoadFilters()
+        {
+            var currencyListJs = _context.Currencies.OrderBy(p => p.Name).AsNoTracking().ToList();
+            ViewData["CurrencyListJs"] = currencyListJs;
+
+            var companiesListJs = FiltersHelper.GetCompaniesFilterList(_context);
+            ViewData["CompanyListJs"] = companiesListJs;
+
+            var datePeriodListJs = DateFilter.GetDateFiltersSelectList();
+            ViewData["DatePeriodListJs"] = datePeriodListJs;
         }
     }
 }
