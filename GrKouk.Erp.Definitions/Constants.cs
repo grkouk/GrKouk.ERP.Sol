dfbd51348812ae0c19c86d22b91381b3d8ce4f81
GrKoukRider <grkouk1966@gmail.com>
2020-05-24 18:26:58 +0300

    Definitions of appsettings keys for info page fields

--- GrKouk.Erp.Definitions/Constants.cs
+++ GrKouk.Erp.Definitions/Constants.cs
@@ -3,8 +3,11 @@
     public static class Constants
     {
         public const string AllCompaniesCodeKey = "ALLCOMPCODE";
-        public const string MainInfoPage = "";
-
-
+        public const string MainInfoPageSumOfMaterialBuys = "SumOfMaterialBuysDf";
+        public const string MainInfoPageSumOfServiceBuys = "SumOfServiceBuysDf";
+        public const string MainInfoPageSumOfExpenseBuys = "SumOfExpenseBuysDf";
+        public const string MainInfoPageSumOfMaterialSales = "SumOfMaterialSalesDf";
+        public const string MainInfoPageSumOfServiceSales = "SumOfServiceSalesDf";
+        public const string MainInfoPageSumOfIncomeSales = "SumOfIncomeSalesDf";
     }
 }
