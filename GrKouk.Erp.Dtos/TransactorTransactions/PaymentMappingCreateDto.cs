457073aaf5b42711aae2bd373e4c8f06fd540313
GrKouk <grkouk@hotmail.com>
2020-06-01 18:08:30 +0300

    Almost saving

---
+++ GrKouk.Erp.Dtos/TransactorTransactions/PaymentMappingCreateDto.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace GrKouk.Erp.Dtos.TransactorTransactions
+{
+    public class PaymentMappingCreateDto
+    {
+        public int DocId { get; set; }
+        private IList<PaymentMappingLineCreateDto> _paymentMappingLines;
+        public virtual IList<PaymentMappingLineCreateDto> PaymentMappingLines
+        {
+            get => _paymentMappingLines ?? (_paymentMappingLines = new List<PaymentMappingLineCreateDto>());
+            set => _paymentMappingLines = value;
+        }
+    }
+    public class PaymentMappingLineCreateDto
+    {
+        public int ReceiptId { get; set; }
+        public decimal AmountUsed { get; set; }
+
+    }
+}
