4a8eb739314df924d041a31bf0928f2108ee40bb
George Koukoudis <grkouk@hotmail.com>
2020-08-15 23:08:50 +0300

    first step

---
+++ GrKouk.Web.ERP/wwwroot/js/indexpage.lib.js
@@ -0,0 +1,36 @@
+﻿//Index Page library functions
+const indexPageLib = {
+    pagerElementsClassName: "page-link",
+    getPageElements: function () {
+        let pageElements = document.getElementsByClassName(this.pagerElementsClassName);
+        return pageElements;
+    },
+    addPagerElementEventListener: function () {
+        let pagerElements = this.getPageElements();
+        Array.from(pagerElements).forEach(item => {
+            item.addEventListener('click',
+                event => {
+                    console.log("From pager event handler");
+                    let pagerAction = event.currentTarget.dataset.pageraction;
+                    if (pagerAction === undefined) {
+                        return;
+                    }
+                    switch (pagerAction) {
+                        case 'GoToFirst':
+                            break;
+                        case 'GoToPrevious':
+                            break;
+                        case 'GoToNext':
+                            break;
+                        case 'GoToLast':
+                            break;
+                        default:
+                    }
+                });
+        });
+
+    },
+    setPagerElementsClassName: function (className) {
+        this.pagerElementsClassName = className;
+    }
+};
