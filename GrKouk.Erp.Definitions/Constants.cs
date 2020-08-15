83ca6f24988237af56c3c1380f072df2cc80265c
George Koukoudis <grkouk@hotmail.com>
2020-08-15 11:18:47 +0300

    Small updates

--- GrKouk.Erp.Definitions/Constants.cs
+++ GrKouk.Erp.Definitions/Constants.cs
@@ -6,10 +6,10 @@
         public const string MainInfoPageSumOfMaterialBuys = "SumOfMaterialBuysDf";
         public const string MainInfoPageSumOfServiceBuys = "SumOfServiceBuysDf";
         public const string MainInfoPageSumOfExpenseBuys = "SumOfExpenseBuysDf";
-        public const string MainInfoPageSumOfFixedAssetBuys = "SumOfFixedAssetBuysDf";
+        public const string MainInfoPageSumOfFixedAssetBuys = "SumOfFixedAstBuysDf";
         public const string MainInfoPageSumOfMaterialSales = "SumOfMaterialSalesDf";
         public const string MainInfoPageSumOfServiceSales = "SumOfServiceSalesDf";
         public const string MainInfoPageSumOfIncomeSales = "SumOfIncomeSalesDf";
-        public const string MainInfoPageSumOfFixedAssetSales = "SumOfFixedAssetSalesDf";
+        public const string MainInfoPageSumOfFixedAssetSales = "SumOfFixedAstSalesDf";
     }
 }
