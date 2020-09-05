77cabbb5905e149d6b173e8304547c762726d50f
GrKouk <grkouk@hotmail.com>
2020-09-05 11:51:01 +0300

    Cross Entries

---
+++ GrKouk.Web.ERP/Pages/Transactions/TransactorTransMng/AddCrossEntry.cshtml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using AutoMapper;
+using GrKouk.Erp.Domain.DocDefinitions;
+using GrKouk.Web.ERP.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using NToastNotify;
+
+namespace GrKouk.Web.ERP.Pages.Transactions.TransactorTransMng
+{
+    [Authorize(Roles = "Admin")]
+    public class AddCrossEntry : PageModel
+    {
+        private readonly ApiDbContext _context;
+        private readonly IMapper _mapper;
+        private readonly IToastNotification _toastNotification;
+        [BindProperty]
+        public CrossEntryDto ItemVm { get; set; }
+        
+        public AddCrossEntry(ApiDbContext context, IMapper mapper, IToastNotification toastNotification)
+        {
+            _context = context;
+            _mapper = mapper;
+            _toastNotification = toastNotification;
+        }
+        public void OnGet()
+        {
+            LoadCombos();
+        }
+        private void LoadCombos()
+        {
+            var transactorsListDb = _context.Transactors
+                .Include(p=>p.TransactorType)
+                .Where(p=>p.TransactorType.Code != "SYS.DTRANSACTOR")
+                .OrderBy(s => s.Name).AsNoTracking();
+            List<SelectListItem> transactorsList = new List<SelectListItem>();
+            
+            foreach (var dbTransactor in transactorsListDb)
+            {
+
+                transactorsList.Add(new SelectListItem() { Value = dbTransactor.Id.ToString(), Text = dbTransactor.Name +"-"+ dbTransactor.TransactorType.Code });
+            }
+            ViewData["CompanyId"] = new SelectList(_context.Companies.OrderBy(c => c.Code).AsNoTracking(), "Id", "Code");
+            ViewData["FiscalPeriodId"] = new SelectList(_context.FiscalPeriods.OrderBy(p => p.Name).AsNoTracking(), "Id", "Name");
+            ViewData["TransactorId"] = new SelectList(transactorsList, "Value", "Text");
+            ViewData["DocSeriesId"] = new SelectList(_context.TransTransactorDocSeriesDefs.OrderBy(s => s.Name).AsNoTracking(), "Id", "Name");
+            //ViewData["SectionId"] = new SelectList(_context.Sections, "Id", "Code");
+        }
+    }
+
+    public class CrossEntryDto
+    {
+        [DataType(DataType.Date)]
+        public DateTime TransDate { get; set; }
+        public int DocSeries1Id { get; set; }
+        public int DocSeries2Id { get; set; }
+        public int Transactor1Id { get; set; }
+        public int Transactor2Id { get; set; }
+        public string RefCode { get; set; }
+        public string Etiology { get; set; }
+        public decimal Amount { get; set; }
+        public int CompanyId { get; set; }
+       
+    }
+}
