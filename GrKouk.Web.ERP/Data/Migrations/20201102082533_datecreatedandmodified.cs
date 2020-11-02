38b8f761787203eb187af9914aa663b38a7ca940
GrKouk <grkouk@hotmail.com>
2020-11-02 10:26:53 +0200

    added migration for datecreated and datemodified

---
+++ GrKouk.Web.ERP/Data/Migrations/20201102082533_datecreatedandmodified.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GrKouk.Web.ERP.Data.Migrations
+{
+    public partial class datecreatedandmodified : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateCreated",
+                table: "WarehouseItems",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateLastModified",
+                table: "WarehouseItems",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateCreated",
+                table: "Transactors",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateLastModified",
+                table: "Transactors",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateCreated",
+                table: "WarehouseItems");
+
+            migrationBuilder.DropColumn(
+                name: "DateLastModified",
+                table: "WarehouseItems");
+
+            migrationBuilder.DropColumn(
+                name: "DateCreated",
+                table: "Transactors");
+
+            migrationBuilder.DropColumn(
+                name: "DateLastModified",
+                table: "Transactors");
+        }
+    }
+}
