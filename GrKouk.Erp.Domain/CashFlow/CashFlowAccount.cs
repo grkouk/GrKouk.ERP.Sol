37affe3ec360586390b4dbeecf3b4de6c5b07de4
GrKouk <grkouk@hotmail.com>
2020-11-05 12:18:19 +0200

    Cash flow section entities

---
+++ GrKouk.Erp.Domain/CashFlow/CashFlowAccount.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using GrKouk.Erp.Domain.DocDefinitions;
+
+namespace GrKouk.Erp.Domain.CashFlow
+{
+    public class CashFlowAccount
+    {
+        public int Id { get; set; }
+
+        [MaxLength(20)]
+        public string Code { get; set; }
+
+        [MaxLength(200)]
+        public string Name { get; set; }
+    }
+}
