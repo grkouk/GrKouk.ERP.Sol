cb5ab66e7bf87666a07c7a3b08d3ac6c273a9806
GrKoukRider <grkouk1966@gmail.com>
2020-12-14 13:14:32 +0200

    Changes in etities for Expenses

--- GrKouk.Erp.Domain/Shared/BuyDocLine.cs
+++ GrKouk.Erp.Domain/Shared/BuyDocLine.cs
@@ -17,6 +17,11 @@ namespace GrKouk.Erp.Domain.Shared
         //public virtual MeasureUnit PrimaryUnit { get; set; }
         public int SecondaryUnitId { get; set; }
         //public virtual MeasureUnit SecondaryUnit { get; set; }
+        //Αλλαγή της εισαγωγής σε νουμερα στην μοναδα κίνησης.
+        //Δηλ 10 συσκευασίες με τιμή συσκευασίας και απο αυτά υπολογισμός σε μοναδα βάσης
+        public int TransactionUnitId { get; set; }
+        public double TransactionQuantity { get; set; }
+        public Single TransactionUnitFactor { get; set; }
         public Single Factor { get; set; }
         /// <summary>
         /// Ποσότητα σε μονάδα μέτρησης 1
@@ -30,14 +35,19 @@ namespace GrKouk.Erp.Domain.Shared
         public decimal DiscountRate { get; set; }
         [Column(TypeName = "decimal(18, 4)")]
         public decimal UnitPrice { get; set; }
+    
         [Column(TypeName = "decimal(18, 4)")]
-        public decimal UnitExpenses { get; set; }
+        public decimal TransUnitPrice { get; set; }
+    
         [Column(TypeName = "decimal(18, 4)")]
         public decimal AmountFpa { get; set; }
         [Column(TypeName = "decimal(18, 4)")]
         public decimal AmountNet { get; set; }
         [Column(TypeName = "decimal(18, 4)")]
         public decimal AmountDiscount { get; set; }
+        [Column(TypeName = "decimal(18, 4)")]
+        public decimal AmountExpenses { get; set; }
+    
         [MaxLength(500)]
         public string Etiology { get; set; }
     }
--- GrKouk.Erp.Dtos/BuyDocuments/BuyDocLineAjaxDto.cs
+++ GrKouk.Erp.Dtos/BuyDocuments/BuyDocLineAjaxDto.cs
@@ -5,10 +5,16 @@ namespace GrKouk.Erp.Dtos.BuyDocuments
     public class BuyDocLineAjaxDto
     {
         public int WarehouseItemId { get; set; }
+        public int TransactionUnitId { get; set; }
+        public double TransactionQuantity { get; set; }
+        public Single TransactionUnitFactor { get; set; }
         public double Q1 { get; set; }
         public double Q2 { get; set; }
         public decimal Price { get; set; }
         public decimal Amount { get; set; }
+        public decimal TransUnitPrice { get; set; }
+        public decimal AmountDiscount { get; set; }
+        public decimal AmountExpenses { get; set; }
         public Single DiscountRate { get; set; }
         public int MainUnitId { get; set; }
         public int SecUnitId { get; set; }
