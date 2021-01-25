c1bc87a21fbf3789041a1949c2675526ebe3bbc1
grkouk <grkouk@hotmail.com>
2021-01-25 17:48:13 +0200

    itemSelector partial and javascript files

---
+++ GrKouk.Web.ERP/wwwroot/js/definitions.js
@@ -0,0 +1,15 @@
+﻿var definitionsLib = (function() {
+    const indexPageTypeEnum = {
+        IndexPage: 1,
+        ProductSelector: 20,
+
+        TransactorSelector: 50,
+        SupplierSelector: 51,
+        CustomerSelector: 52
+       
+    };
+    Object.freeze(indexPageTypeEnum);
+    return {
+        IndexPageTypeEnum:indexPageTypeEnum
+    };
+})();
