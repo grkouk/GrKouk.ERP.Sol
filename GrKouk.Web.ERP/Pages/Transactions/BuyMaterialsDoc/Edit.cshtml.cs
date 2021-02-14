63a99f2864715943ed0c7538dbc1af9a6e683ab7
GrKoukRider <grkouk1966@gmail.com>
2021-02-14 13:11:43 +0200

    Step

--- GrKouk.Web.ERP/Pages/Transactions/BuyMaterialsDoc/Edit.cshtml.cs
+++ GrKouk.Web.ERP/Pages/Transactions/BuyMaterialsDoc/Edit.cshtml.cs
@@ -90,33 +90,7 @@ namespace GrKouk.Web.ERP.Pages.Transactions.BuyMaterialsDoc
             ViewData["PaymentMethodId"] = new SelectList(_context.PaymentMethods.OrderBy(p => p.Name).AsNoTracking(), "Id", "Name");
         }
 
-        //public async Task<IActionResult> OnPostAsync()
-        //{
-        //    //if (!ModelState.IsValid)
-        //    //{
-        //    //    return Page();
-        //    //}
-
-        //    //_context.Attach(ItemVm).State = EntityState.Modified;
-
-        //    //try
-        //    //{
-        //    //    await _context.SaveChangesAsync();
-        //    //}
-        //    //catch (DbUpdateConcurrencyException)
-        //    //{
-        //    //    if (!BuyMaterialsDocumentExists(ItemVm.Id))
-        //    //    {
-        //    //        return NotFound();
-        //    //    }
-        //    //    else
-        //    //    {
-        //    //        throw;
-        //    //    }
-        //    //}
-
-        //    //return RedirectToPage("./Index2");
-        //}
+       
 
         private bool BuyMaterialsDocumentExists(int id)
         {
