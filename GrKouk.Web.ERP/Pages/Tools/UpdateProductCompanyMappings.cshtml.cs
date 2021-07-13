f158a8f8d2afc894fe6980d252b50a664f14f0a7
GrKouk <grkouk@hotmail.com>
2021-07-13 22:50:47 +0300

    Some more fixes

--- GrKouk.Web.ERP/Pages/Tools/UpdateProductCompanyMappings.cshtml.cs
+++ GrKouk.Web.ERP/Pages/Tools/UpdateProductCompanyMappings.cshtml.cs
@@ -17,29 +17,53 @@ namespace GrKouk.Web.ERP.Pages.Tools
         {
             _context = context;
         }
+        public IList<string> ResultsList { get; set; }=new List<string>();
+        public int allCount { get; set; } = 0;
+        public int updatedCount { get; set; } = 0;
+        public int errorCount { get; set; } = 0;
         public void OnGet()
         {
         }
         public void OnPost()
         {
+           
             var wiList = _context.WarehouseItems;
+
             foreach (var item in wiList)
             {
+                
                 var wrId = item.Id;
-                var cmpId = item.CompanyId;
+                var cmpId = item.CompanyId==0?1:item.CompanyId;
+                string itemLine = $"item {item.Name} with Id {item.Id} and companyId {item.CompanyId}";
+                string itemResult = "";
                 var mpList = _context.CompanyWarehouseItemMappings.Where(p=>p.CompanyId==cmpId && p.WarehouseItemId == wrId).ToList();
-                if (mpList.Count == 0)
+                if (mpList != null)
                 {
-                    var cmpMap = new CompanyWarehouseItemMapping()
+                    if (mpList.Count == 0)
                     {
-                        CompanyId = cmpId,
-                        WarehouseItemId = wrId
-                    };
-                    _context.CompanyWarehouseItemMappings.Add(cmpMap);
-                   
+                        var cmpMap = new CompanyWarehouseItemMapping()
+                        {
+                            CompanyId = cmpId,
+                            WarehouseItemId = wrId
+                        };
+                        try
+                        {
+                            _context.CompanyWarehouseItemMappings.Add(cmpMap);
+                            _context.SaveChanges();
+                            updatedCount++;
+                        }
+                        catch (Exception ex)
+                        {
+                            itemResult = $"Error with {itemLine} error is {ex.Message}";
+                            ResultsList.Add(itemResult);
+                            errorCount++;
+                        }
+                      
+                    }
                 }
+                allCount++;
             }
-            _context.SaveChanges();
+            
         }
     }
 }
