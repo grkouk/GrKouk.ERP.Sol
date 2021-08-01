df17d653172b6fc3ba6cb237113be28bc986edb5
GrKouk <grkouk@hotmail.com>
2021-08-01 23:12:18 +0300

    BuyDocTransamount migraitions

--- GrKouk.Web.ERP/Data/Migrations/20210801200605_BuyDocTransAmounts.Designer.cs
+++ GrKouk.Web.ERP/Data/Migrations/20210801200605_BuyDocTransAmounts.Designer.cs
@@ -4,14 +4,16 @@ using GrKouk.Web.ERP.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace GrKouk.Web.ERP.Data.Migrations
 {
     [DbContext(typeof(ApiDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210801200605_BuyDocTransAmounts")]
+    partial class BuyDocTransAmounts
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
