f81e8348bca23948e4a0dc325f73069fd3cf9fc9
GrKouk <grkouk@hotmail.com>
2021-08-01 22:58:55 +0300

    Added trans amounts

--- GrKouk.Erp.Domain/Shared/BuyDocument.cs
+++ GrKouk.Erp.Domain/Shared/BuyDocument.cs
@@ -38,6 +38,17 @@ namespace GrKouk.Erp.Domain.Shared
         [Column(TypeName = "decimal(18, 4)")]
         public decimal AmountDiscount { get; set; }
 
+        [Column(TypeName = "decimal(18, 4)")]
+        public decimal AmountExpenses { get; set; }
+
+        [Column(TypeName = "decimal(18, 4)")]
+        public decimal TransFpaAmount { get; set; }
+        [Column(TypeName = "decimal(18, 4)")]
+        public decimal TransNetAmount { get; set; }
+        [Column(TypeName = "decimal(18, 4)")]
+        public decimal TransDiscountAmount { get; set; }
+        [Column(TypeName = "decimal(18, 4)")]
+        public decimal TransExpensesAmount { get; set; }
         [MaxLength(500)]
         public string Etiology { get; set; }
 
