36fd5ae4b637f6745c28ca5f6028cd04b434d147
grkouk <grkouk@hotmail.com>
2022-01-15 18:46:56 +0200

    Step

--- GrKouk.Web.ERP/Helpers/IndexFilterResult.cs
+++ GrKouk.Web.ERP/Helpers/IndexFilterResult.cs
@@ -24,6 +24,7 @@ namespace GrKouk.Web.ERP.Helpers
         public bool ShowCompaniesFlt { get; set; } = true;
         public bool ShowPageSizeFlt { get; set; } = true;
         public bool ShowDisplayCarryOnLineFlt { get; set; } = false;
+        public bool ShowDisplaySumaryFlt { get; set; } = false;
 
     }
 }
