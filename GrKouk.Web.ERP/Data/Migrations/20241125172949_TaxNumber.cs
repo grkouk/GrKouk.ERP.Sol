60a2e1d726e7de8ec8cd971c923283a2b46935d1
George Koukoudis <grkouk@hotmail.com>
2024-11-25 19:34:48 +0200

    TaxNumber migration

---
+++ GrKouk.Web.ERP/Data/Migrations/20241125172949_TaxNumber.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GrKouk.Web.ERP.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class TaxNumber : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TaxNumber",
+                table: "Transactors",
+                type: "nvarchar(15)",
+                maxLength: 15,
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactors_TaxNumber",
+                table: "Transactors",
+                column: "TaxNumber",
+                unique: true,
+                filter: "[TaxNumber] IS NOT NULL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Transactors_TaxNumber",
+                table: "Transactors");
+
+            migrationBuilder.DropColumn(
+                name: "TaxNumber",
+                table: "Transactors");
+        }
+    }
+}
