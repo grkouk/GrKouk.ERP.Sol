a64f1783b80e4b9e6858ac6068b0ebfc2d8aeacf
George Koukoudis <grkouk@hotmail.com>
2024-11-25 18:47:34 +0200

    Added TaxNumber to Transactor Entity and Dto classes

--- GrKouk.Erp.Dtos/Transactors/TransactorCreateDto.cs
+++ GrKouk.Erp.Dtos/Transactors/TransactorCreateDto.cs
@@ -6,7 +6,8 @@ namespace GrKouk.Erp.Dtos.Transactors
     {
         [MaxLength(15)]
         public string Code { get; set; }
-
+        [MaxLength(15)]
+        public string TaxNumber { get; set; }
         [MaxLength(200)]
         public string Name { get; set; }
 
--- GrKouk.Erp.Dtos/Transactors/TransactorDetailDto.cs
+++ GrKouk.Erp.Dtos/Transactors/TransactorDetailDto.cs
@@ -4,6 +4,8 @@
     {
         public int Id { get; set; }
         public string Code { get; set; }
+        
+        public string TaxNumber { get; set; }
         public string Name { get; set; }
         public string TransactorTypeName { get; set; }
         public string TransactorTypeCode { get; set; }
