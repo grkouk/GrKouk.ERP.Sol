3e4eac826d1b708437cfa35556f68761b70e4bb3
George Koukoudis <grkouk@hotmail.com>
2025-01-22 01:32:52 +0200

    Added Average

--- GrKouk.Web.ERP/Pages/Settings/AppSettings.cshtml.cs
+++ GrKouk.Web.ERP/Pages/Settings/AppSettings.cshtml.cs
@@ -34,7 +34,16 @@ namespace GrKouk.Web.ERP.Pages.Settings
                 new AppSetting {Code = GrKouk.Erp.Definitions.Constants.MainInfoPageSumOfServiceSales, Value = ""},
                 new AppSetting {Code = GrKouk.Erp.Definitions.Constants.MainInfoPageSumOfIncomeSales, Value = ""},
                 new AppSetting {Code = GrKouk.Erp.Definitions.Constants.MainInfoPageSumOfFixedAssetBuys, Value = ""},
-                new AppSetting {Code = GrKouk.Erp.Definitions.Constants.MainInfoPageSumOfFixedAssetSales, Value = ""}
+                new AppSetting {Code = GrKouk.Erp.Definitions.Constants.MainInfoPageSumOfFixedAssetSales, Value = ""},
+                new AppSetting {Code=GrKouk.Erp.Definitions.Constants.MainInfoPageAverageOfMaterialBuys,Value = ""},
+                new AppSetting {Code=GrKouk.Erp.Definitions.Constants.MainInfoPageAverageOfExpenseBuys,Value = ""},
+                new AppSetting {Code=GrKouk.Erp.Definitions.Constants.MainInfoPageAverageOfServiceBuys,Value = ""},
+                new AppSetting {Code=GrKouk.Erp.Definitions.Constants.MainInfoPageAverageOfMaterialSales,Value = ""},
+                new AppSetting {Code=GrKouk.Erp.Definitions.Constants.MainInfoPageAverageOfServiceSales,Value = ""},
+                new AppSetting {Code=GrKouk.Erp.Definitions.Constants.MainInfoPageAverageOfIncomeSales,Value = ""},
+                new AppSetting {Code=GrKouk.Erp.Definitions.Constants.MainInfoPageAverageOfFixedAssetBuys,Value = ""},
+                new AppSetting {Code=GrKouk.Erp.Definitions.Constants.MainInfoPageAverageOfFixedAssetSales,Value = ""},
+                
             };
         }
 
