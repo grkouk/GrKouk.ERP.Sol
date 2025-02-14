fc491d7263c7ac8f1116098472528d59ebe5e011
George Koukoudis <grkouk@hotmail.com>
2025-02-14 23:13:10 +0200

    Step

---
+++ GrKouk.Web.ERP/Data/Migrations/20250213225307_RefreshTokensAdditionalProperties.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GrKouk.Web.ERP.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RefreshTokensAdditionalProperties : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedByIp",
+                table: "RefreshTokens",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                table: "RefreshTokens",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedByIp",
+                table: "RefreshTokens");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                table: "RefreshTokens");
+        }
+    }
+}
