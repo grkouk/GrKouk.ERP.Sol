3cb907258aac100443c8872f0cda9d780c3f1900
George Koukoudis <grkouk@hotmail.com>
2025-04-17 09:19:08 +0300

    Migration

---
+++ GrKouk.Web.ERP/Data/Migrations/20250416072723_SyncSupplierAddTaxNumber.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GrKouk.Web.ERP.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SyncSupplierAddTaxNumber : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TaxNumber",
+                table: "SyncSuppliers",
+                type: "nvarchar(25)",
+                maxLength: 25,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TaxNumber",
+                table: "SyncSuppliers");
+        }
+    }
+}
