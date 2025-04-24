f2648111e660d2f2cc7d5f84ffb3a647f27356f7
George Koukoudis <grkouk@hotmail.com>
2025-04-24 23:33:39 +0300

    Added new migration

---
+++ GrKouk.Web.ERP/Data/Migrations/20250424203039_SyncDocumentsPart2.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GrKouk.Web.ERP.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SyncDocumentsPart2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BuyDocDefName",
+                table: "SyncSaleDocument",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CustomerName",
+                table: "SyncSaleDocument",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "BuyDocDefName",
+                table: "SyncBuyDocument",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SupplierName",
+                table: "SyncBuyDocument",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BuyDocDefName",
+                table: "SyncSaleDocument");
+
+            migrationBuilder.DropColumn(
+                name: "CustomerName",
+                table: "SyncSaleDocument");
+
+            migrationBuilder.DropColumn(
+                name: "BuyDocDefName",
+                table: "SyncBuyDocument");
+
+            migrationBuilder.DropColumn(
+                name: "SupplierName",
+                table: "SyncBuyDocument");
+        }
+    }
+}
