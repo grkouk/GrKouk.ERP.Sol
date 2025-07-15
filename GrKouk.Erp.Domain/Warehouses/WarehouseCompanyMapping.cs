67f15fbf30acd56d061f658a017a922625bc6ca3
George Koukoudis <grkouk@hotmail.com>
2025-07-15 21:11:12 +0300

    Added warehouse domain classes

---
+++ GrKouk.Erp.Domain/Warehouses/WarehouseCompanyMapping.cs
@@ -0,0 +1,12 @@
+﻿using GrKouk.Erp.Domain.Shared;
+
+namespace GrKouk.Erp.Domain.Warehouses
+{
+    public class WarehouseCompanyMapping
+    {
+        public int WarehouseId { get; set; }
+        public Warehouse Warehouse { get; set; }
+        public int CompanyId { get; set; }
+        public Company Company { get; set; }
+    }
+}
