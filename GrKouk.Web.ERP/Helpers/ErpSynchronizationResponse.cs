0b953ca060eaa015845208e8384b29568e90e645
George Koukoudis <grkouk@hotmail.com>
2025-08-13 20:43:33 +0300

    Refactored `ErpCheckDocumentResponse` into a separate file and added `IsChanged` property

--- GrKouk.Web.ERP/Helpers/ErpSynchronizationResponse.cs
+++ GrKouk.Web.ERP/Helpers/ErpSynchronizationResponse.cs
@@ -29,16 +29,3 @@ public class ErpSynchronizationResponse<T>
     [JsonPropertyName("syncItems")]
     public List<T> SyncItems { get; set; }
 }
-
-public class ErpCheckDocumentResponse
-{
-    [JsonPropertyName("isSynced")] 
-    public bool IsSynced { get; set; } = false;
-
-    [JsonPropertyName("canSync")] 
-    public bool CanSync { get; set; } = false;
-    [JsonPropertyName("message")]
-    public string Message { get; set; }
-    [JsonPropertyName("documentId")]
-    public int DocumentId { get; set; }
-}
