c7016ec07eb27cc459510b3d0f9fb4d1270812a8
George Koukoudis <grkouk@hotmail.com>
2025-09-25 23:45:40 +0300

    Changed company filter to comapnies filter

--- GrKouk.Web.ERP/Controllers/GrkoukInfoApiController.cs
+++ GrKouk.Web.ERP/Controllers/GrkoukInfoApiController.cs
@@ -6566,18 +6566,22 @@ namespace GrKouk.Web.ERP.Controllers
                     transListBeforePeriod = transListBeforePeriod.Where(p => p.TransDate < fromDate);
                 }
             }
-
-            if (!string.IsNullOrEmpty(request.CompanyFilter))
+            if (!string.IsNullOrEmpty(request.CompaniesFilter))
             {
-                if (int.TryParse(request.CompanyFilter, out var companyId))
-                {
-                    if (companyId > 0)
-                    {
-                        transactionsList = transactionsList.Where(p => p.CompanyId == companyId);
-                        transListAll = transListAll.Where(p => p.CompanyId == companyId);
-                    }
-                }
+                transactionsList = FilterEval.ApplyIntListFilterFromJson(transactionsList, request.CompaniesFilter, 0, p => p.CompanyId);
+                transListAll = FilterEval.ApplyIntListFilterFromJson(transListAll, request.CompaniesFilter, 0, p => p.CompanyId);
             }
+            // if (!string.IsNullOrEmpty(request.CompanyFilter))
+            // {
+            //     if (int.TryParse(request.CompanyFilter, out var companyId))
+            //     {
+            //         if (companyId > 0)
+            //         {
+            //             transactionsList = transactionsList.Where(p => p.CompanyId == companyId);
+            //             transListAll = transListAll.Where(p => p.CompanyId == companyId);
+            //         }
+            //     }
+            // }
 
             if (!string.IsNullOrEmpty(request.SearchFilter))
             {
@@ -6670,19 +6674,22 @@ namespace GrKouk.Web.ERP.Controllers
             decimal runningTotal = 0;
             if (request.ShowCarryOnAmountsInTabs)
             {
-               
-                 if (!string.IsNullOrEmpty(request.CompanyFilter))
-                 {
-                     if (int.TryParse(request.CompanyFilter, out var companyId))
-                     {
-                         if (companyId > 0)
-                         {
-                            
-                                 transListBeforePeriod = transListBeforePeriod.Where(p => p.CompanyId == companyId);
-                            
-                         }
-                     }
-                 }
+                if (!string.IsNullOrEmpty(request.CompaniesFilter))
+                {
+                    transListBeforePeriod = FilterEval.ApplyIntListFilterFromJson(transListBeforePeriod, request.CompaniesFilter, 0, p => p.CompanyId);
+                }
+                 // if (!string.IsNullOrEmpty(request.CompanyFilter))
+                 // {
+                 //     if (int.TryParse(request.CompanyFilter, out var companyId))
+                 //     {
+                 //         if (companyId > 0)
+                 //         {
+                 //            
+                 //                 transListBeforePeriod = transListBeforePeriod.Where(p => p.CompanyId == companyId);
+                 //            
+                 //         }
+                 //     }
+                 // }
 
                  if (!string.IsNullOrEmpty(request.SearchFilter))
                  {
@@ -7029,19 +7036,23 @@ namespace GrKouk.Web.ERP.Controllers
             //Handle before period carry on row
             if (request.ShowCarryOnAmountsInTabs)
             {
-                if (!string.IsNullOrEmpty(request.CompanyFilter))
+                if (!string.IsNullOrEmpty(request.CompaniesFilter))
                 {
-                    if (int.TryParse(request.CompanyFilter, out var companyId))
-                    {
-                        if (companyId > 0)
-                        {
-                            if (request.ShowCarryOnAmountsInTabs)
-                            {
-                                transListBeforePeriod = transListBeforePeriod.Where(p => p.CompanyId == companyId);
-                            }
-                        }
-                    }
+                    transListBeforePeriod = FilterEval.ApplyIntListFilterFromJson(transListBeforePeriod, request.CompaniesFilter, 0, p => p.CompanyId);
                 }
+                // if (!string.IsNullOrEmpty(request.CompanyFilter))
+                // {
+                //     if (int.TryParse(request.CompanyFilter, out var companyId))
+                //     {
+                //         if (companyId > 0)
+                //         {
+                //             if (request.ShowCarryOnAmountsInTabs)
+                //             {
+                //                 transListBeforePeriod = transListBeforePeriod.Where(p => p.CompanyId == companyId);
+                //             }
+                //         }
+                //     }
+                // }
 
                 if (!string.IsNullOrEmpty(request.SearchFilter))
                 {
